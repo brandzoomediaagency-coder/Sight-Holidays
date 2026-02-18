efbf26079f460a12829b865a826eef3cabe4fa41
brandzoomediaagency <brandzoomediaagency@gmail.com>
2026-02-18 13:42:13 +0530

    Initial commit - Sight Holidays luxury travel website

---
+++ server.ps1
@@ -0,0 +1,40 @@
+$l = [System.Net.HttpListener]::new()
+$l.Prefixes.Add("http://localhost:8081/")
+try {
+    $l.Start()
+    Write-Host "Server started on http://localhost:8081/"
+    while ($l.IsListening) {
+        $c = $l.GetContext()
+        $urlPath = $c.Request.Url.LocalPath
+        if ($urlPath -eq "/") { $urlPath = "/index.html" }
+        $p = Join-Path (Get-Location) ($urlPath.TrimStart('/'))
+        
+        if (Test-Path $p -PathType Leaf) {
+            $ext = [System.IO.Path]::GetExtension($p).ToLower()
+            $contentType = switch ($ext) {
+                ".html" { "text/html" }
+                ".css" { "text/css" }
+                ".js" { "application/javascript" }
+                ".jpg" { "image/jpeg" }
+                ".jpeg" { "image/jpeg" }
+                ".png" { "image/png" }
+                ".svg" { "image/svg+xml" }
+                default { "application/octet-stream" }
+            }
+            $c.Response.ContentType = $contentType
+            $b = [System.IO.File]::ReadAllBytes($p)
+            $c.Response.ContentLength64 = $b.Length
+            $c.Response.OutputStream.Write($b, 0, $b.Length)
+        }
+        else {
+            $c.Response.StatusCode = 404
+        }
+        $c.Response.Close()
+    }
+}
+catch {
+    Write-Error $_
+}
+finally {
+    $l.Stop()
+}
